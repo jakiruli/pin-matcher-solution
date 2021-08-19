a7769f4d0a0b2c3545ab9675727d7c45349b490f
jakiruli <jakiruleng@gmail.com>
2021-08-19 16:43:47 +0600

    changed

--- js/app.js
+++ js/app.js
@@ -14,8 +14,51 @@
  }
 function generatePin(){
     const pin = getPin();
-    document.getElementById('display-pin').value = pin;
-   
-    
+    document.getElementById('display-pin').value = pin;  
 
 }
+/* genaratePin function end */ 
+
+/* key-pad , typed-number button using even bubble  start   */
+
+document.getElementById('key-pad').addEventListener('click' , function(event){
+    const number = event.target.innerText;
+    const calcInput = document
+    .getElementById('typed-numbers');
+
+    if(isNaN(number)){
+        if(number){
+            calcInput.value = '';
+        }
+    }
+    else{
+        
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+    }
+});
+/* key-pad , typed-number button using even bubble  end   */
+
+/* verify match generate and submit  start */
+function verifyPin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+
+    const successMessage = document.getElementById('notify-success');
+
+    const failError = document.getElementById('notify-fail');
+
+    if(pin == typedNumbers){
+
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+
+    }
+    else{
+        
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+};
+/* verify match generate and submit  end */
